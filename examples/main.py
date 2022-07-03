c7a6501cd5e82dbeac290f3b4882e4d9d47157d3
mc_fdc <Masato.04.11.2007@gmail.com>
2022-07-03 05:01:01 +0000

    ok

--- examples/main.py
+++ examples/main.py
@@ -1,9 +1,10 @@
 from ugc_sdk import Client
 
 import asyncio
+from os import getenv
 
 async def main():
     client = Client(loop=asyncio.get_running_loop())
-    await client.connect("token")
+    await client.connect(getenv("token"))
 
 asyncio.run(main())
